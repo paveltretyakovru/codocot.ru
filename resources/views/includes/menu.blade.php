40103f943645457563334a44a722ac323cafd4ee
Tretyakov Pavel <tretjakov.pavel@gmail.com>
2015-08-05 22:19:36 +0300

    Разработка дизайна  Changes to be committed: 	new file:   public/css/system/menu.css 	new file:   public/css/system/sidebar.css 	modified:   resources/views/includes/menu.blade.php 	new file:   resources/views/includes/sidebar.blade.php 	modified:   resources/views/master.blade.php

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,13 +1,20 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Codocot</a>
-    </div>
-  </div>
+<nav class="navbar navbar-default no-margin palette-emerald">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header fixed-brand">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"  id="menu-toggle">
+            <span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+            <i class="fa fa-rocket fa-4"></i> CODOCOT
+        </a>
+    </div><!-- navbar-header-->
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li class="active" >
+                <button class="navbar-toggle collapse in" data-toggle="collapse" id="menu-toggle-2">
+                </button>
+            </li>
+        </ul>
+    </div><!-- bs-example-navbar-collapse-1 -->
 </nav>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,22 +9,36 @@
 
     {{-- Plugins level --}}
     {!! HTML::style('/bower_components/pace/themes/green/pace-theme-minimal.css') !!}
+
+    {{-- Tretyakov Pavel level --}}
+    {!! HTML::style('css/system/sidebar.css') !!}
 </head>
 
 
 <body>
+    @include('includes.menu')
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                @include('includes.menu')
-            </div>
 
-            <div class="col-md-12">
-                @yield('content')
+<div id="wrapper">
+
+    @include('includes.sidebar')
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                     @yield('content')
+                </div>
             </div>
         </div>
-    </div>	
+    </div>
+    <!-- /#page-content-wrapper -->
+
+</div>
+    <!-- /#wrapper -->
+
+   
 
 	{!! HTML::script('/bower_components/requirejs/require.js') !!}
     {!! HTML::script('js/app/config.js') !!}
@@ -39,7 +53,37 @@
 
     		window.app = app || false;
     		app.start();
-    	});
+
+            $("#menu-toggle").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+            $("#menu-toggle-2").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled-2");
+                $('#menu ul').hide();
+            });
+
+            function initMenu() {
+                $('#menu ul').hide();
+                $('#menu ul').children('.current').parent().show();
+                //$('#menu ul:first').show();
+                $('#menu li a').click(
+                    function() {
+                        var checkElement = $(this).next();
+                        if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+                            return false;
+                        }
+                        if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+                            $('#menu ul:visible').slideUp('normal');
+                            checkElement.slideDown('normal');
+                            return false;
+                        }
+                    }
+                );
+            }
+            $(document).ready(function() {initMenu();});
+        });
     </script>
 </body>
 </html>
