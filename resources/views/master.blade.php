02135878efb29a0e04fb0de2064a372e8eafe4c4
Pavel Tretyakov <tretjakov.pavel@gmail.com>
2015-08-13 18:59:58 +0300

    Подключен виджет VK сообщества

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -79,8 +79,14 @@
   </div><!-- /.col-md-8 -->
 
   <div class="col-md-4 sidebar-block">
-    <div class="row row_margin">
-      test
+    <div class="row row_margin" style="margin:auto">
+
+      <script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
+      <!-- VK Widget -->
+      <div id="vk_groups"></div>
+      <script type="text/javascript">
+      VK.Widgets.Group("vk_groups", {mode: 2, width: "300", height: "400"}, 100009050);
+      </script>
     </div>
   </div>
 </div><!-- /.container -->
